1a219ce237ba84758366fce7d7e98dbdeec1cf66
Edwin Cevallos <edwin_paul01@hotmail.com>
2023-05-06 22:06:58 -0500

    Sección sobre creada

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,20 @@
-const menuShow = document.querySelector(".container__head");
-const menuAction = document.querySelector(".menu__hamburguer");
+(function() {
+  const menuShow = document.querySelector(".container__head");
+  const menuAction = document.querySelector(".menu__hamburguer");
+  
+  menuAction.addEventListener("click", ()=> {
+    menuShow.classList.toggle("show")
+  })
 
-menuAction.addEventListener("click", ()=> {
-  menuShow.classList.toggle("show")
-})
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+      });
+    });
+  });
+
+
+})()
