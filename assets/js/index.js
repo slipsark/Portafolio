117376d3e84b1a9b504fc25714133a93fe4804e7
Edwin Cevallos <edwin_paul01@hotmail.com>
2023-05-09 17:51:36 -0500

    Creacion del componente up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,13 @@
 (function() {
   const menuShow = document.querySelector(".container__head");
   const menuAction = document.querySelector(".menu__hamburguer");
+  const up = document.querySelector(".up");
   
   menuAction.addEventListener("click", ()=> {
     menuShow.classList.toggle("show")
   })
 
-  document.querySelectorAll('a[href^="#"]').forEach(link => {
+  /* document.querySelectorAll('a[href^="#"]').forEach(link => {
   link.addEventListener('click', function (e) {
     e.preventDefault();
 
@@ -14,7 +15,18 @@
       behavior: 'smooth'
       });
     });
+  }); */
+
+  up.addEventListener("click", ()=> {
+    document.documentElement.scrollTop = 0;
   });
 
+  window.addEventListener("scroll", ()=> {
+    document.documentElement.scrollTop === 0 
+      ? 
+      up.style.display = "none"
+      :
+      up.style.display = "flex"
+  })
 
 })()
